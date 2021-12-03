4ad611e5a45e989f5a9fb0de630c55d1351436da
Jorge Duron <duronj08@gmail.com>
2021-12-03 11:36:06 -0600

    https

--- apiconfig/apiIndex.js
+++ apiconfig/apiIndex.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import {apiK} from "./key.js"
 export const getLocationInit = axios.create({
-    baseURL: 'http://api.openweathermap.org/data/2.5/weather',
+    baseURL: 'https://api.openweathermap.org/data/2.5/weather',
     params:{
         APPID: apiK.apiKey,
         units: 'metric'
