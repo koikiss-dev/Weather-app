ee72f2642df23fdca578caead2c344732a32a466
Jorge Duron <duronj08@gmail.com>
2021-12-03 19:07:53 -0600

    d

--- hooks/useGetLocation.js
+++ hooks/useGetLocation.js
@@ -62,7 +62,7 @@ const useGetLocation = () => {
       getData(lat, lng);
     }
     console.log(process.env.SECRET_KEY);
-    setInterval(ubicacion, 20000);
+    setInterval(ubicacion, 2000);
   }, [getData, lat, lng, value]);
 
   return [data, value,loading, lat, lng, pushData, getData];
